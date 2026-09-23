07007635ac08a78f5209e2c9f6e738c59c31dd73
dvakbn <161695365+dvakbn@users.noreply.github.com>
2026-09-23 10:44:29 +0530

    Reposition question hint to right corner and add one-click calendar reminders

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Sadhanapada service worker: keeps the app working offline */
-const CACHE='sadhanapada-v1';
+const CACHE='sadhanapada-v2';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));
